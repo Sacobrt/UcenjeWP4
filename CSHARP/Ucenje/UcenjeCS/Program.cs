992014ca06db229d12843546acdac5b8b3187152
Sacobrt <sacerdanijel@gmail.com>
2024-06-04 18:08:35 +0200

    Isprobavanje ispisa

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+ * 
+ * Komentar kroz
+ * više redova
+ * 
+ */
 Console.WriteLine("Hello, World!");
+Console.WriteLine(7);
+Console.WriteLine(true);
+Console.WriteLine("Danijel");
