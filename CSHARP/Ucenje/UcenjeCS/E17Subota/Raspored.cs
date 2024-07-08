9b5b4b9499309bab4e959e44a7c749b9fd70ce7f
Sacobrt <sacerdanijel@gmail.com>
2024-07-08 08:22:50 +0200

    Promjenjen datum početka

--- CSHARP/Ucenje/UcenjeCS/E17Subota/Raspored.cs
+++ CSHARP/Ucenje/UcenjeCS/E17Subota/Raspored.cs
@@ -9,14 +9,14 @@ namespace UcenjeCS.E17Subota
 
             // u ljetim mjesecima svakih mjesec dana, a u zimskim svaka dva tjedna
 
-            DateTime datumOd = DateTime.Parse("2024-07-08");
+            DateTime datumOd = DateTime.Parse("2024-07-15");
             DateTime datumDo = datumOd;
             DateTime tjedan = datumOd;
             int broj = 0;
 
             while (datumOd.Year < 2028)
             {
-                if (tjedan.Month >= 4 && tjedan.Month <= 10)
+                if (tjedan.Month < 10 && tjedan.Month > 4)
                 {
                     datumDo = datumOd.AddDays(27);
                 }
@@ -24,16 +24,22 @@ namespace UcenjeCS.E17Subota
                 {
                     datumDo = datumOd.AddDays(13);
                 }
-                if (datumOd.Month == 10 && datumDo.Month == 11)
-                {
-                    datumDo = datumOd.AddDays(20);
-                }
                 tjedan = datumOd.AddDays(6);
 
                 Console.WriteLine("{0} - {1}, Stan {2}",
                     datumOd.ToString("dd.MM.yyyy."),
-                    tjedan.ToString("dd.MM.yyyy."), ++broj % 3 + 1);
+                    tjedan.ToString("dd.MM.yyyy."), Stan(++broj));
                 datumOd = datumDo.AddDays(1);
+            }
+        }
+        private int Stan(int broj)
+        {
+            switch (broj % 3 + 1)
+            {
+                case 2: return 1;
+                case 3: return 2;
+                case 1: return 3;
+                default: return 1;
             }
         }
     }
