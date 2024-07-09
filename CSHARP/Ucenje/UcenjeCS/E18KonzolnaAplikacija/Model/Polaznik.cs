6a5101ff1741e5cda4a799cf80f83d8da027d45e
Sacobrt <sacerdanijel@gmail.com>
2024-07-09 18:56:00 +0200

    Nastava konzolna aplikacija

---
+++ CSHARP/Ucenje/UcenjeCS/E18KonzolnaAplikacija/Model/Polaznik.cs
@@ -0,0 +1,11 @@
+﻿
+namespace UcenjeCS.E18KonzolnaAplikacija.Model
+{
+    internal class Polaznik : Entitet
+    {
+        public string? Ime { get; set; }
+        public string? Prezime { get; set; }
+        public string? OIB { get; set; }
+        public string? Email { get; set; }
+    }
+}
--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -8,7 +8,8 @@ using UcenjeCS.E13KlasaObjekt;
 //LjubavniKalkulator.Izvedi();
 
 //Učenje
-new UcenjeCS.E17Subota.Raspored();
+new UcenjeCS.E18KonzolnaAplikacija.Izbornik();
+//new UcenjeCS.E17Subota.Raspored();
 //new UcenjeCS.E17Subota.Program();
 //new UcenjeCS.E16GenericiLambdaEkstenzije.Program();
 //new UcenjeCS.E15RadSStringovima.Program();
