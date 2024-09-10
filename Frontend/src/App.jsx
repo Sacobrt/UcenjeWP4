b3979367eec8d4bd0cc280b80830c36d986a1f74
Sacobrt <sacerdanijel@gmail.com>
2024-09-10 18:56:13 +0200

    Završena priprema react za spjanje na API

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import NavBarEdunova from './components/NavBarEdunova'
+import { Route, Routes } from 'react-router-dom'
+import { RoutesNames } from './constants'
+import Pocetna from './pages/Pocetna'
+import SmjeroviPregled from './pages/smjerovi/SmjeroviPregled'
 
 function App() {
 
@@ -8,6 +12,10 @@ function App() {
   return (
     <>
       <NavBarEdunova />
+      <Routes>
+        <Route path={RoutesNames.HOME} element={<Pocetna />} />
+        <Route path={RoutesNames.SMJER_PREGLED} element={<SmjeroviPregled />} />
+      </Routes>
     </>
   )
 }
