34e87c89e8a2eb49c447d4d06af19661cee90ad9
Sacobrt <sacerdanijel@gmail.com>
2024-10-04 16:47:42 +0200

    nedostatak backend-a

---
+++ CSHARP/Ucenje/EdunovaAPP/Mapping/EdunovaMappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using EdunovaAPP.Models;
+using EdunovaAPP.Models.DTO;
+
+namespace EdunovaAPP.Mapping
+{
+    public class EdunovaMappingProfile:Profile
+    {
+        public EdunovaMappingProfile()
+        {
+            // kreiramo mapiranja: izvor, odredište
+            CreateMap<Smjer, SmjerDTORead>();
+            CreateMap<SmjerDTOInsertUpdate, Smjer>();
+
+            CreateMap<Polaznik, PolaznikDTORead>();
+            CreateMap<PolaznikDTOInsertUpdate, Polaznik>();
+
+            CreateMap<Grupa, GrupaDTORead>()
+                .ForMember(
+                    dest => dest.SmjerNaziv,
+                    opt => opt.MapFrom(src => src.Smjer.Naziv)
+                );
+            CreateMap<Grupa, GrupaDTOInsertUpdate>().ForMember(
+                    dest => dest.SmjerSifra,
+                    opt => opt.MapFrom(src => src.Smjer.Sifra)
+                );
+            CreateMap<GrupaDTOInsertUpdate, Grupa>();
+
+        }
+    }
+}
---
+++ CSHARP/Ucenje/EdunovaAPP/Models/Polaznik.cs
@@ -0,0 +1,12 @@
+﻿namespace EdunovaAPP.Models
+{
+    public class Polaznik:Entitet
+    {
+        public string? Ime { get; set; }
+        public string? Prezime { get; set; }
+        public string? Oib { get; set; }
+        public string? Email { get; set; }
+
+        public ICollection<Grupa>? Grupe { get; } = [];
+    }
+}
